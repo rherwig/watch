5b8cb5d1b51ad19634e8ba5b623e2d4a9390cc0d
Rico Herwig <rherwig4711@gmail.com>
2021-09-26 07:14:34 +0200

    Add temporary development landing page

--- src/shared/pages/Index.vue
+++ src/shared/pages/Index.vue
@@ -1,37 +1,16 @@
 <template>
-    <Headline>Join a room</Headline>
-    <input
-        type="text"
-        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-        placeholder="Room name"
-        v-model="roomName"
-    />
-    <Button
-        class="mt-4"
-        @click="joinRoom"
-    >
-        Join
-    </Button>
+    <div class="bg-gray-900 h-full w-full flex items-center justify-center">
+        <div class="text-gray-100 text-center">
+            <h1 class="text-7xl font-extrabold mb-8">
+                Greetings, stranger!
+            </h1>
+            <p class="text-2xl">
+                This site is currently under development. But you might want to check back later.
+            </p>
+        </div>
+    </div>
 </template>
 
 <script setup>
-import { inject, ref } from 'vue';
-
-import * as events from '../socket/events';
-import Headline from '../components/typography/headlines/Headline.vue';
-import Button from '../components/typography/buttons/Button.vue';
-
-const roomName = ref('');
-const socket = inject('socket');
-
-const joinRoom = () => {
-    socket.emit(events.ROOM_JOIN_TRY, {
-        name: roomName,
-    });
-
-    socket.on(events.ROOM_JOIN_CONFIRM, () => {
-        console.log('Room found!');
-    });
-};
 
 </script>
