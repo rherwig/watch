rherwig/watch

commit b640eeff2804a6c9472d14d5a03ab22db6408b06

Move socket logic to vuex store

src/shared/components/players/Player.vue
@@ -6,21 +6,19 @@
 </template>
 
 <script setup>
-import { onMounted, ref, watch } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
+import { useStore } from 'vuex';
 
-import { events, useSocket } from '../../socket';
+import * as playerState from '@/constants/youtube-player';
 
-const emit = defineEmits([
-    'ready',
-    'destroyed',
-    'play',
-    'pause',
-    'stop',
-]);
+const store = useStore();
 
-const socket = useSocket();
-let player = ref(null);
-const playerState = ref(-1);
+const player = ref(null);
+const isPlayPending = ref(false);
+
+const videoId = computed(() => store.state.video.id);
+const remoteVideoState = computed(() => store.state.video.remoteState);
+const localVideoState = computed(() => store.state.video.localState);
 
 const createPlayer = (videoId) => {
     const options = {
@@ -30,17 +28,16 @@ const createPlayer = (videoId) => {
         playerVars: {
             playsinline: 1,
             enablejsapi: 1,
-            autoplay: 1,
+            autoplay: 0,
         },
         events: {
             onReady: (event) => {
                 player.value = event.target;
-                player.value.stopVideo();
-                emit('ready');
             },
             onStateChange: (event) => {
-                playerState.value = event.data;
-                console.log(event.target);
+                store.dispatch('video/setLocalState', {
+                    state: event.data,
+                });
             },
         },
     };
@@ -48,30 +45,51 @@ const createPlayer = (videoId) => {
     new YT.Player('player', options);
 };
 
-const loadVideo = (videoId) => {
+watch(videoId, (nextVideoId) => {
     if (player.value) {
-        emit('destroyed');
         player.value.destroy();
     }
 
     player.value = null;
 
-    createPlayer(videoId);
-};
+    createPlayer(nextVideoId);
+});
 
-watch(playerState, (state, prevState) => {
-    console.log(`From ${prevState} to ${state}`);
+watch(localVideoState, (state, prevState) => {
+    console.log(`[LOCAL] From ${prevState} to ${state}`);
+
     switch (state) {
-        case 0:
-            emit('stop');
+        case playerState.ENDED:
+            store.dispatch('video/stop');
             break;
-        case 1:
-            emit('play', {
+        case playerState.PLAYING:
+            store.dispatch('video/play', {
                 currentTime: player.value.getCurrentTime(),
             });
             break;
-        case 2:
-            emit('pause');
+        case playerState.PAUSED:
+            store.dispatch('video/pause');
+            break;
+        default:
+            break;
+    }
+});
+
+watch(remoteVideoState, (state, prevState) => {
+    console.log(`[REMOTE] From ${prevState} to ${state}`);
+
+    switch (state) {
+        case playerState.ENDED:
+            player.value.stopVideo();
+
+            break;
+        case playerState.PLAYING:
+            player.value.playVideo();
+
+            break;
+        case playerState.PAUSED:
+            player.value.pauseVideo();
+
             break;
         default:
             break;
@@ -79,42 +97,33 @@ watch(playerState, (state, prevState) => {
 });
 
 onMounted(() => {
-    socket.on(events.VIDEO_LOAD, (payload) => {
-        const { videoId } = payload;
-        if (!videoId) {
-            return;
-        }
-
-        loadVideo(videoId);
-    });
-
-    socket.on(events.VIDEO_PLAY, (payload) => {
-        if (playerState.value === 1) {
-            return;
-        }
-
-        const { currentTime } = payload;
-        if (currentTime) {
-            player.value.seekTo(currentTime);
-        }
-
-        player.value.playVideo();
-    });
-
-    socket.on(events.VIDEO_PAUSE, () => {
-        if (playerState.value === 2) {
-            return;
-        }
-
-        player.value.pauseVideo();
-    });
-
-    socket.on(events.VIDEO_STOP, () => {
-        if (playerState.value === 0) {
-            return;
-        }
-
-        player.value.stopVideo();
-    });
+    // socket.on(events.VIDEO_PLAY, (payload) => {
+    //     if (playerState.value === 1) {
+    //         return;
+    //     }
+    //
+    //     const { currentTime } = payload;
+    //     if (currentTime) {
+    //         player.value.seekTo(currentTime);
+    //     }
+    //
+    //     player.value.playVideo();
+    // });
+    //
+    // socket.on(events.VIDEO_PAUSE, () => {
+    //     if (playerState.value === 2) {
+    //         return;
+    //     }
+    //
+    //     player.value.pauseVideo();
+    // });
+    //
+    // socket.on(events.VIDEO_STOP, () => {
+    //     if (playerState.value === 0) {
+    //         return;
+    //     }
+    //
+    //     player.value.stopVideo();
+    // });
 });
 </script>

ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
             ignore_watch: [
                 'node_modules',
                 'var/**/*',
-                'dist'
+                'dist',
             ],
             watch_options: {
                 followSymlinks: false,

src/client/index.js
@@ -1,6 +1,9 @@
 import createApp from '../shared';
+import createSocketPlugin from './store/plugins/socket';
 
-const { app, router, store } = createApp();
+const { app, router, store } = createApp({
+    storePlugins: [createSocketPlugin()],
+});
 
 /**
  * Hydrates the server-side rendered app with the client app,

src/client/store/plugins/socket/index.js
@@ -1,0 +1,55 @@
+import io from 'socket.io-client';
+
+/**
+ * When on the client-side, create a socket connecting to the backend.
+ *
+ * @return {import('socket.io-client').Socket}
+ */
+const createSocket = () => {
+    return io(undefined, {
+        reconnection: true,
+        rejectUnauthorized: false,
+        transports: ['websocket'],
+    });
+};
+
+/**
+ * Returns all mutations matching the event.
+ *
+ * @param store
+ * @param event
+ * @returns {string[]}
+ */
+const findMutations = (store, event) => {
+    return Object.keys(store._mutations).filter((mutation) => {
+        return mutation.split('/').pop() === event;
+    });
+};
+
+/**
+ * Creates a Vuex plugin that listens to socket events on the client-side.
+ *
+ * @returns {(function(*): void)|*}
+ */
+export default () => {
+    /**
+     * @param {import('vuex').Store<any>} store
+     */
+    return (store) => {
+        if (import.meta.env.SSR) {
+            return;
+        }
+
+        const socket = createSocket();
+
+        socket.onAny((event, payload) => {
+            const mutations = findMutations(store, event);
+
+            mutations.forEach((mutation) => {
+                store.commit(mutation, payload);
+            });
+        });
+
+        store.$socket = socket;
+    };
+};

src/shared/components/layout/header/Header.vue
@@ -19,7 +19,7 @@
             <input
                 type="text"
                 class="shadow-sm text-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-100 dark:border-gray-700 dark:placeholder-gray-400"
-                placeholder="Enter a watch ID"
+                placeholder="Enter a video URL"
                 v-model="videoLink"
             />
             <Button
@@ -38,19 +38,20 @@
 <script setup>
 import { computed, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { useStore } from 'vuex';
 
-import { events, useSocket } from '../../../socket';
 import logo from '../../../assets/images/logo.png';
 import Button from '../../ui/buttons/Button.vue';
 import Switch from '../../ui/buttons/Switch.vue';
 
 const route = useRoute();
-const socket = useSocket();
+const store = useStore();
+
 const videoLink = ref('');
+const isDarkMode = ref(true);
 const isSearchVisible = computed(() => {
     return route.name === 'room';
 });
-const isDarkMode = ref(true);
 
 const handleDarkMode = (value) => {
     isDarkMode.value = value;
@@ -66,7 +67,7 @@ const load = () => {
     const idRegex = /([A-Za-z0-9_\-]{11})($|&|\?)/;
     const videoId = videoLink.value.match(idRegex)[1];
 
-    socket.emit(events.VIDEO_LOAD_REQUEST, {
+    store.dispatch('video/load', {
         videoId,
     });
 

src/shared/components/room/RoomOverview.vue
@@ -11,7 +11,6 @@
             <div class="border-b border-gray-200 dark:border-gray-800">
                 <div class="px-6">
                     <nav class="-mb-px flex space-x-6">
-                        <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" -->
                         <a
                             href="#"
                             class="border-indigo-500 text-indigo-600 whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm dark:text-indigo-200 dark:border-indigo-300"
@@ -21,15 +20,15 @@
                     </nav>
                 </div>
             </div>
-            <ul role="list" class="flex-1 divide-y divide-gray-200 overflow-y-auto">
+            <ul role="list" class="flex-1 divide-y divide-gray-200 dark:divide-gray-800 overflow-y-auto">
                 <li
-                    v-for="user in users.values()"
+                    v-for="user in users"
                     :key="user.id"
                 >
                     <div class="relative group py-6 px-5 flex items-center">
                         <a href="#" class="-m-1 flex-1 block p-1">
                             <div
-                                class="absolute inset-0 group-hover:bg-gray-50"
+                                class="absolute inset-0 group-hover:bg-gray-50 dark:group-hover:bg-gray-800"
                                 aria-hidden="true"
                             ></div>
                             <div class="flex-1 flex items-center min-w-0 relative">
@@ -50,7 +49,7 @@
                                         {{ user.name }}
                                     </p>
                                     <p
-                                        v-if="user.isMe"
+                                        v-if="me.id === user.id"
                                         class="text-sm text-gray-500 truncate dark:text-gray-400"
                                     >
                                         you
@@ -60,85 +59,17 @@
                         </a>
                     </div>
                 </li>
-
-                <!-- More people... -->
             </ul>
         </div>
     </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 
-import { events, useSocket } from '../../socket';
+const store = useStore();
 
-const route = useRoute();
-const socket = useSocket();
-
-const { name } = route.params;
-const isJoined = ref(false);
-const users = ref(new Map());
-const chatMessages = ref([]);
-const chatMessage = ref('');
-
-onMounted(() => {
-    socket.emit(events.ROOM_JOIN_TRY, {
-        name,
-    });
-
-    socket.on(events.ROOM_JOIN_CONFIRM, (payload) => {
-        if (payload.name !== name) {
-            return;
-        }
-
-        isJoined.value = true;
-    });
-
-    socket.on(events.ROOM_USER_JOINED, (payload) => {
-        const { room, user } = payload;
-
-        if (room !== name) {
-            return;
-        }
-
-        users.value.set(user.id, user);
-
-        socket.emit(events.ROOM_SYNC, {
-            user,
-            room,
-        });
-    });
-
-    socket.on(events.ROOM_USER_LEFT, (payload) => {
-        const { id } = payload.user;
-
-        users.value.delete(id);
-    });
-
-    socket.on(events.ROOM_USER_UPDATE, (payload) => {
-        payload.users.forEach((user) => {
-            if (user.id === socket.id) {
-                user.isMe = true;
-            }
-
-            users.value.set(user.id, user);
-        });
-    });
-
-    socket.on(events.CHAT_MESSAGE_RECEIVED, (payload) => {
-        chatMessages.value.push({
-            user: payload.user,
-            message: payload.message,
-        });
-    });
-});
-
-const sendMessage = () => {
-    socket.emit(events.CHAT_MESSAGE_SEND, {
-        message: chatMessage.value,
-    });
-
-    chatMessage.value = '';
-};
+const users = computed(() => store.state.users.all);
+const me = computed(() => store.getters['users/me']);
 </script>

src/shared/constants/youtube-player.js
@@ -1,0 +1,6 @@
+export const UNSTARTED = -1;
+export const ENDED = 0;
+export const PLAYING = 1;
+export const PAUSED = 2;
+export const BUFFERING = 3;
+export const CUED = 5;

src/shared/index.js
@@ -3,20 +3,21 @@ import { createSSRApp, h } from 'vue';
 import './main.scss';
 
 import createRouter from './router';
-import { createSocket, SOCKET_KEY } from './socket';
 import createStore from './store';
 import App from './App.vue';
 
-export default () => {
+export default (config = {}) => {
+    const { storePlugins } = config;
+
     const router = createRouter();
-    const socket = createSocket();
-    const store = createStore();
+    const store = createStore({
+        plugins: storePlugins,
+    });
 
     const app = createSSRApp({
         render: () => h(App),
     });
 
-    app.provide(SOCKET_KEY, socket);
     app.use(router);
     app.use(store);
 

commit 78e09e3c7d0588d59cb64f19cb3f28137f6ca9e6

Fix the bug introduced in commit b640eef that broke the functionality of skipping ahead in videos and syncing the new play time.

src/shared/components/players/Player.vue
@@ -84,6 +84,9 @@ watch(remoteVideoState, (state, prevState) => {
 
             break;
         case playerState.PLAYING:
+            if (store.state.video.currentTime) {
+                player.value.seekTo(store.state.video.currentTime);
+            }
             player.value.playVideo();
 
             break;